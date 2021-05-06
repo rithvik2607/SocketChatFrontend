273781c222b518f27f9cf57031b7544c32be365f
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2021-05-07 00:40:36 +0530

    Minor corrections and added README

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Input = ({ message, setMessage, sendMessage }) => {
   return(
-    <form className="container mx-auto flex items-center justify-center">
+    <form className="container mx-auto px-1 flex items-center justify-center">
       <input
-        className="h-8 w-5/12 pl-2 focus:outline-none"
+        className="h-9 w-4/12 pl-2 my-0.5 focus:outline-none "
         type="string"
         placeholder="Send a message...."
         value={message}
         onChange = {({target: {value} }) => setMessage(value)}
         onKeyPress = {event => event.key === 'Enter' ? sendMessage(event) : null }
       />
-      <button className="text-white py-1 px-6 bg-blue-400 focus:outline-none" onClick={(event) => sendMessage(event)}>Send</button>
+      <button className="text-white py-1.5 px-12 bg-blue-400 focus:outline-none" onClick={(event) => sendMessage(event)}>Send</button>
     </form>
   );
 }
